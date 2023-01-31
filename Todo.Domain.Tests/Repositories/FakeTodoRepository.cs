350627ea5d0b0f7e3080b016be6569314aa1c2ba
Pedro Gois <pedrohgois@gmail.com>
2023-01-30 22:49:07 -0300

    Initial commit

---
+++ Todo.Domain.Tests/Repositories/FakeTodoRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Domain.Entities;
+using Todo.Domain.Repositories;
+
+namespace Todo.Domain.Tests.Repositories
+{
+    public class FakeTodoRepository : ITodoRepository
+    {
+        public void Create(TodoItem todo)
+        {
+           
+        }
+
+        public TodoItem GetById(Guid id, string user)
+        {
+            return new TodoItem("Titulo", "Pedro", DateTime.Now);
+        }
+
+        public void Update(TodoItem todo)
+        {
+           
+        }
+    }
+}
